05eeb2a8ea76f53e42baf8dc82daa5be918afb43
Ivan Godwin <igodwin@pivotal.io>
2019-11-01 00:10:22 +0000

    initial add of dotnet/sslputget

--- cpp/sslputget/main.cpp
+++ cpp/sslputget/main.cpp
@@ -3,7 +3,6 @@
 #include <geode/Cache.hpp>
 #include <geode/CacheFactory.hpp>
 #include <geode/PoolManager.hpp>
-
 #include <geode/RegionShortcut.hpp>
 
 int main(int argc, char** argv) {
@@ -11,11 +10,9 @@ int main(int argc, char** argv) {
   properties->insert("log-level", "debug");
   properties->insert("ssl-enabled", "true");
   properties->insert("ssl-strict-mode", "true");
-//  properties->insert("ssl-truststore", "/home/igodwin/Workspace/gemfire-nativeclient-examples/sslputget/ClientSslKeys/mynew.pem");
-//  properties->insert("ssl-truststore", "/home/igodwin/Workspace/gemfire-nativeclient-examples/sslputget/ClientSslKeys/client_truststore.pem");
-   properties->insert("ssl-truststore", "/home/igodwin/Workspace/gemfire-nativeclient-examples/sslputget/ClientSslKeys/client_truststore_chained_root.pem");
-  // properties->insert("ssl-keystore", "/home/igodwin/Workspace/gemfire-nativeclient-examples/sslputget/ClientSslKeys/client_keystore.password.pem");
-  // properties->insert("ssl-keystore-password", "gemstone");
+  properties->insert("ssl-keystore", "<path to keystore>");
+  properties->insert("ssl-keystore-password", "<keystore password>");
+  properties->insert("ssl-truststore", "<path to truststore>");
 
   apache::geode::client::CacheFactoryPtr cacheFactory = apache::geode::client::CacheFactory::createCacheFactory(properties);
   apache::geode::client::CachePtr cachePtr = cacheFactory->create();
@@ -26,8 +23,8 @@ int main(int argc, char** argv) {
   apache::geode::client::CacheablePtr valuePtr = apache::geode::client::CacheableString::create("123");
   regionPtr->put(keyPtr, valuePtr);
 
-  apache::geode::client::CacheablePtr result1Ptr = regionPtr->get(keyPtr);
-  std::cout << "I gots me some of them " << result1Ptr->toString()->toString() << "\n";
+  apache::geode::client::CacheablePtr resultPtr = regionPtr->get(keyPtr);
+  std::cout << "I gots me some of them " << resultPtr->toString()->toString() << "\n";
 
   cachePtr->close();
 }
---
+++ dotnet/sslputget/Program.cs
@@ -0,0 +1,37 @@
+﻿using Apache.Geode.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sslputget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CacheFactory cacheFactory = CacheFactory.CreateCacheFactory();
+            Cache cache = cacheFactory.Set("cache-xml-file", "client_config.xml")
+		    .Set("log-level", "debug")
+		    .Set("log-file", "sslputget.log")
+		    .Set("ssl-enabled", "true")
+		    .Set("ssl-strict-mode", "true")
+		    .Set("ssl-keystore", "<path to keystore>")
+		    .Set("ssl-keystore-password", "<password>")
+		    .Set("ssl-truststore", "<path to truststore>")
+		    .Create();
+
+            IRegion<int, string> region = cache.GetRegion<int, string>("testSSLRegion");
+
+            region[0] = "zero";
+            Console.WriteLine("Put key/value in region.");
+
+            string result = region[0];
+
+            Console.WriteLine("Retrieved the value \"{0}\" from the region.", result);
+
+            cache.Close();
+        }
+    }
+}
